13ef130014010685ed1a50b24b3e585991b030b1
Linus Torvalds <torvalds@linux-foundation.org>
2025-05-01 00:19:33 +0300

    Dockerize

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM rust as builder
+WORKDIR /app
+COPY . .
+RUN cargo build --release
+
+FROM rust as runner
+WORKDIR /app
+COPY --from=builder /app/target/release/url-shortener /app/
+COPY migrations /app/
+CMD ["./url-shortener"]
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,64 @@
 mod handlers;
 mod utils;
-
+use std::num::ParseIntError;
 use std::os::linux::raw::stat;
 use crate::utils::api::responses::BodyBuilder;
 use actix_web::web::Data;
 use actix_web::{get, App, HttpResponse, HttpServer};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
+use std::fmt::Debug;
+#[derive(Debug)]
+pub enum Error {
+    EnvNotFound,
+    ParseIntError
+}
+
+impl From<std::env::VarError> for Error {
+    fn from(value: std::env::VarError) -> Self {
+        Error::EnvNotFound
+    }
+}
+
+impl From<ParseIntError> for Error {
+    fn from(value: ParseIntError) -> Self {
+        Error::ParseIntError
+    } 
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+pub struct EnvironmentVars {
+    database_url: String,
+    shortener_url: String,
+    port: String
+}
+fn get_env_vars() -> Result<EnvironmentVars> {
+    Ok(EnvironmentVars {
+        database_url: std::env::var("DATABASE_URL")?,
+        shortener_url: std::env::var("SHORTENER_URL")?,
+        port: std::env::var("PORT").unwrap_or("3000".to_string())
+    })
+}
 
 pub struct AppState {
     shortener_url: String,
-    db_client: PgPool
+    db_client: PgPool,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().expect("Dotenv failed to initialize.");
-    let db_url = std::env::var("DATABASE_URL").expect("Expected DATABASE_URL environment variable to be set.");
+    let env_vars = match get_env_vars() {
+        Ok(vars) => vars,
+        Err(_) => {
+            dotenv::dotenv().expect("Dotenv failed to initialize.");
+            get_env_vars().expect("Say whaat")
+        }
+
+    };
+
     let state = Data::new(AppState {
-        shortener_url: std::env::var("SHORTENER_URL").unwrap(),
-        db_client: PgPoolOptions::new().max_connections(10).connect(&db_url).await.unwrap()
+        shortener_url: env_vars.shortener_url,
+        db_client: PgPoolOptions::new().max_connections(10).connect(&env_vars.database_url).await.unwrap()
     });
 
     sqlx::migrate!("./migrations").run(&state.db_client).await.expect("Could not run migrations.");
@@ -30,7 +69,7 @@ async fn main() -> std::io::Result<()> {
             .configure(handlers::configure)
             .service(health)
     })
-        .bind(format!("0.0.0.0:{}", std::env::var("PORT").unwrap_or("3000".to_string())))?
+        .bind(format!("0.0.0.0:{}", env_vars.port))?
         .run()
         .await
 }
